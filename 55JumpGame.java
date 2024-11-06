c92d55bf2818fb65b5d9ddef36fe00229c004537
Rohan Kumar Sharma <rkrk02233@gmail.com>
2024-11-06 14:36:49 +0530

    updated

--- 55JumpGame.java
+++ 55JumpGame.java
@@ -1,1 +1,23 @@
-class
+class Solution {
+  public boolean canJump(int[] nums) {
+      int n = nums.length;
+      int Rechable = 0;
+      for(int i=0;i<n;i++)
+      {
+          
+      if(i>Rechable)
+      {
+              return false;
+      }
+       Rechable = Math.max(Rechable,i+nums[i]);
+       if(Rechable>= nums.length-1)
+       {
+          return true;
+       }
+ 
+      }
+
+      return false;
+      
+  }
+}
